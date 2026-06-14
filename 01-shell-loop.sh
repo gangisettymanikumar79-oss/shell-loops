3df4882206c276a88216dd02f00f02b712444ca8
.email.com <gangisettymani98@gmail.com>
2026-06-14 15:54:47 +0530

    i want create new file.so if i create new file

---
+++ 01-shell-loop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+UserID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+# check the root access or not
+if [ $UserID -ne 0 ]; then
+   echo "please run the script with proper reason"
+   exit 1
+fi
+for package in $@
+if [ $? -ne 0 ]; then
+    do 
+        echo "installing $package"
+        dnf list installing $package
+    done
+
+
